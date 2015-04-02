bac5ec9c7a0984472451d21f8c1705ef724b9cc0
Prashant Singh Pawar <prashantpawar@gmail.com>
2015-04-01 21:38:31 -0400

    -Added regex for capturing and sorting the file names into proper order of their chapter names

--- index.js
+++ index.js
@@ -5,13 +5,22 @@
  */
 
 var program = require('commander'),
-    glob = require('glob');
+    glob = require('glob'),
+    options,
+    re = /ch(\d*).html/g; 
+    
 
 program
   .version('0.0.1')
   .option('-b --bookfolder [path]', 'Path to the book folder', '.')
   .parse(process.argv);
 
-if (program.bookfolder) {
-    console.log(program.bookfolder);
-}
+options = {
+    cwd: program.bookfolder,
+    nosort: true
+};
+glob('**/ch*.html', options, function (er, files) {
+    files.map(function (current, index) {
+        console.log(current, index);
+    });
+});
